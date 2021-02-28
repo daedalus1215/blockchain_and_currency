2fbc7d064a11a40a39692c6669a96621a7c2229f
larry adams <ladams1776@yahoo.com>
2021-02-27 22:03:44 -0500

    add the RQ for getting the full blockchain

--- blockchain.py
+++ blockchain.py
@@ -62,3 +62,29 @@ class Blockchain:
             
         
         
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work[previous_proof]
+    blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message', 'Congratulations, you just mined a block!', 
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain, 
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+        
