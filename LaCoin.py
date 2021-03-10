c2d7b73ec2be264a68d8cb632e47771b37edebff
larry adams <ladams1776@yahoo.com>
2021-03-10 08:26:27 -0500

    Update add_transaction fill in it's functionality

--- LaCoin.py
+++ LaCoin.py
@@ -144,10 +144,16 @@
         response = {'isChainValid': blockchain.is_chain_valid(blockchain.chain)}
         return jsonify(response), 200
     
+    # Adding a new transaction to the Blockchain
     @app.route('add_transaction', methods=['POST'])
     def add_transaction():
         json = request.get_json()
-        
+        transaction_key = ['sender', 'receiver', 'amount']
+        if not all (key in json for key in transaction_keys):
+            return 'Some elements of the transaction are missing', 400
+       index = blockchain.add_transactions(json['sender'], json['receiver'], json['amount']) 
+       response = {'message': f'This transaction will be added to Block {index}'}
+       return jsonify(response), 201
         
         
         
