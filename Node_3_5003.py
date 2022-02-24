94b43c75158f06554094c1961db989682aa214a7
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-02-24 12:24:03 -0500

    Migrate Node 3 over to the Blockchain class

--- Node_3_5003.py
+++ Node_3_5003.py
@@ -1,139 +1,21 @@
-import datetime
-import hashlib
-import json
-from time import time
-from typing import List
+from uuid import uuid4
 
 from flask import Flask, jsonify, request
-import requests
-from uuid import uuid4, UUID
-from urllib.parse import urlparse
-
-
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.transactions = []
-        self.create_block(proof=1, previous_hash='0')
-        self.nodes = set()
-        self.utxo_set = List
-
-    def create_block(self, proof, previous_hash):
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': str(datetime.datetime.now()),
-            'proof': proof,
-            'previous_hash': previous_hash,
-            'transactions': self.transactions
-        }
-        # must reset transactions after adding to the block, since we
-        # cannot write the same transactions to a brand new block.
-        self.transactions = []
-        self.chain.append(block)
-        return block
-
-    def get_previous_block(self):
-        return self.chain[-1]
-
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
-        check_proof = False
-        while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
-            print(f"hash_operatuion: {hash_operation}")
-            print(f"hash_operation[:4]: {hash_operation[:4]}")
-            if hash_operation[:4] == '0000':
-                check_proof = True
-            else:
-                new_proof += 1
-        return new_proof
-
-    def hash(self, block):
-        print("block")
-        print(block)
-        encoded_block = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
-
-    def is_chain_valid(self, chain):
-        previous_block = chain[0]
-        block_index = 1
-        while block_index < len(chain):
-            block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
-                return False
-            previous_proof = previous_block['proof']
-            print(previous_proof)
-            proof = block['proof']
-            print(proof)
-            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
-            # if hash_operation[:4] != '0000':
-            # Their solution does not validate correctly. Going to stub true.
-            # return False
-            previous_block = block
-            block_index += 1
-        return True
-
-    def get_address(self):
-        return node_address
-
-    def add_transaction(self, from_address, to_address, amount) -> int:
-        """
-
-        :param from_address:
-        :param to_address:
-        :param amount: Amount we want to send
-        :return: current blocks index
-        """
-        # @TODO: Make sure we can spend
-        self.get_amount_for_wallet()
-        self.transactions.append({'txid': uuid4(),
-                                  'amount': amount,
-                                  'from_address': from_address,
-                                  'to_address': to_address,
-                                  'timestamp': time()})
-
-        previous_block = self.get_previous_block()
-        return previous_block['index'] + 1
-
-    def get_amount_for_wallet(self) -> int:
-        print(self.get_previous_block())
-        return 0
-
-    def add_node(self, nodeAddress):
-        parsed_url = urlparse(nodeAddress)
-        self.nodes.add(parsed_url.netloc)
-
-    def replace_chain(self):
-        """
-        
-        :return:
-        """
-        network = self.nodes
-        longest_chain = None
-        max_length = len(self.chain)
-        for node in network:
-            response = requests.get(f'http://{node}/get_chain')
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-                if length > max_length and self.is_chain_valid(chain):
-                    max_length = length
-                    longest_chain = chain
-        if longest_chain:
-            self.chain = longest_chain
-            return True
-        return False
 
 # Part 2 - Mining our blockchain
 
 # Creating a web app
+from Blockchain import Blockchain
+
 app = Flask('blockchain')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 # Creating an address for the node on Port 5000
 node_address = str(uuid4()).replace('-', '')
 
-blockchain = Blockchain()
+HOST = '127.0.0.1'
+PORT = '5003'
+blockchain = Blockchain(f"http://{HOST}:{PORT}")
 
 
 # Mining a new block
@@ -220,4 +102,4 @@ def replace_chain():
 
 # Part 3 - Decentralizing our Blockchain
 # Running the app
-app.run(host='127.0.0.1', port=5003)
+app.run(host=HOST, port=PORT)
